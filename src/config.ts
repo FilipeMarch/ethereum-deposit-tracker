891c66e017b04de4325e17d1cc669d722cbb83f2
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-07-11 22:57:29 -0300

    🚨 Applied prettier to `config.ts`

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-export const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY!
-export const BEACON_DEPOSIT_CONTRACT_ADDRESS = "0x00000000219ab540356cBB839Cbe05303d7705Fa";
+export const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY!;
+export const BEACON_DEPOSIT_CONTRACT_ADDRESS =
+  '0x00000000219ab540356cBB839Cbe05303d7705Fa';
