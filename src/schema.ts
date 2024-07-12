52747483e7b15e1210f4dab8c9972ba7fab7c04c
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-07-12 02:44:50 -0300

    ✨ feat: Updated `blockTimestamp` type on `deposits` table

--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,11 @@
-import { text, pgTable } from 'drizzle-orm/pg-core';
+import { text, pgTable, timestamp } from 'drizzle-orm/pg-core';
 
 export const deposits = pgTable('deposits', {
   hash: text('hash').primaryKey(),
   blockNumber: text('blockNumber').notNull(),
-  blockTimestamp: text('blockTimestamp').notNull(),
+  blockTimestamp: timestamp('blockTimestamp', {
+    withTimezone: true,
+  }).notNull(),
   fee: text('fee'),
   pubkey: text('pubkey').notNull(),
 });
