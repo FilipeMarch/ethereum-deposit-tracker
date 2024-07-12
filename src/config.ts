7b71524eb61295b858657a82bfde6d742812b09c
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-07-11 22:52:33 -0300

    ✨ feat: Added `config.ts` file with two constants: - ALCHEMY_API_KEY (from .env) - BEACON_DEPOSIT_CONTRACT_ADDRESS

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+export const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY!
+export const BEACON_DEPOSIT_CONTRACT_ADDRESS = "0x00000000219ab540356cBB839Cbe05303d7705Fa";
