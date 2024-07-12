6bcb421891b7fa009aa00256122e26bbd7808266
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-07-12 05:38:15 -0300

    ✨ feat: Executing migrations before starting the `tracker`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+import { Logger } from './logger';
+import { doMigration } from './migrate';
 import { startTrackingDeposits } from './tracker';
 
-startTrackingDeposits();
+const logger = new Logger();
+
+async function main() {
+  try {
+    logger.info('Executing migrations');
+    await doMigration();
+
+    logger.info('Starting tracker...');
+    startTrackingDeposits();
+  } catch (error) {
+    logger.error(error);
+  }
+}
+
+main();
